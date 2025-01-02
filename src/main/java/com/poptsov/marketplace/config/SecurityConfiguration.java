7643179b8c0038cecf158c6bdb114e611ff45b11
Poptsov Roman <poptsov.roman@mail.ru>
2025-01-02 17:16:33 +0500

    Remove Bad changeset. Reset DB. add localhost in CORS policy

--- src/main/java/com/poptsov/marketplace/config/SecurityConfiguration.java
+++ src/main/java/com/poptsov/marketplace/config/SecurityConfiguration.java
@@ -71,7 +71,8 @@ public class SecurityConfiguration {
                 List.of("http://77.222.40.238:80", // не помню что это, но работает, лучше не трогать
                         "http://rubronameg.temp.swtest.ru", // домен просто по приколу
                         "http://35.227.137.151:80", // рабочий
-                        "http://176.59.11.125:8080")); // тест
+                        "http://localhost:8080", // тестовый
+                        "http://176.59.11.125:8080")); // тестовый
         corsConfig.setAllowCredentials(true);
         corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         corsConfig.setAllowedHeaders(List.of("Authorization", "Content-Type"));
